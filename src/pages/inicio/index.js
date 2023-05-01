3d89d52702c898e858f1d256dc9d1ffdba21d7dc
Pedro Henrique Gondim <107958899+Pedrohdsg@users.noreply.github.com>
2023-05-01 17:15:53 -0300

    Add files via upload

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,10 +1,11 @@
 import Banner from "components/Banner";
 import styles from "./Inicio.module.css";
+import App from "App";
 
 function Inicio() {
   return (
   <>
-  
+  <App></App>
   </>);
 }
 
